1862deeed563831c86ef239b4478fbd99ca0fc4e
Raul Cayo <raul.sanchez.cayo@gmail.com>
2019-05-20 14:34:18 -0500

    Changed route movies-seen for movie-seen

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ api.add_resource(TokenRefresh, "/refresh")
 
 api.add_resource(MovieAdd, "/movie")
 api.add_resource(Movie, "/movie/<int:movie_id>")
-api.add_resource(MovieSeen, "/movies-seen/<int:movie_id>")
+api.add_resource(MovieSeen, "/movie-seen/<int:movie_id>")
 
 # Run the API from this file
 if __name__ == '__main__':
