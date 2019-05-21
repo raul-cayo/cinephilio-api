beda9a0f0ae2a3f79a7246c4959135c4fa29d925
Raul Cayo <raul.sanchez.cayo@gmail.com>
2019-05-21 17:01:40 -0500

    Added list comprehension for list of movies_seen

--- models/user.py
+++ models/user.py
@@ -49,7 +49,7 @@ class UserModel(db.Model):
 
     def get_movies_seen(self):
         return {
-            "movies": self.movies_seen
+            "movies": [movie.json() for movie in self.movies_seen]
         }
 
     def save_to_db(self):
