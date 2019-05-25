94641eb53ca56c9369f01371e8c4ab8f389956a6
Raul Cayo <raul.sanchez.cayo@gmail.com>
2019-05-24 22:02:40 -0500

    More details in list

--- models/user.py
+++ models/user.py
@@ -49,7 +49,8 @@ class UserModel(db.Model):
 
     def get_movies_seen(self):
         return {
-            "movies_seen": [movie.json() for movie in self.movies_seen]
+            "movies_seen":
+                [movie.movie_seen.json() for movie in self.movies_seen]
         }
 
     def save_to_db(self):
